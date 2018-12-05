64208b9b16c522d550c0bec7134610c070b3e646
Julien SECHAUD <juliensechaud@gmail.com>
2018-12-05 23:09:24 +0100

    Init CoreBluetooth

--- happnBLE/MasterViewController.swift
+++ happnBLE/MasterViewController.swift
@@ -7,15 +7,68 @@
 //
 
 import UIKit
+import CoreBluetooth
+
+
+struct Peripheral {
+    var cbPeripheral: CBPeripheral
+    var advertisementData: [String : Any]
+}
+
+
+extension MasterViewController: CBPeripheralDelegate {
+    
+    func peripheral(_ peripheral: CBPeripheral, didDiscoverServices error: Error?) {
+        guard let services = peripheral.services else {return}
+        for service in services {
+            print("service : \(service)")
+        }
+    }
+    
+}
+
+extension MasterViewController: CBCentralManagerDelegate {
+    func centralManager(_ central: CBCentralManager, didDiscover peripheral: CBPeripheral, advertisementData: [String : Any], rssi RSSI: NSNumber) {
+        guard discoveredPeripherals.filter({$0.cbPeripheral == peripheral}).count == 0 else {return}
+        print("didDiscover : \(peripheral.name ?? "UNKNOWN")")
+        discoveredPeripherals.append(Peripheral(cbPeripheral: peripheral, advertisementData: advertisementData))
+    }
+    
+    func centralManager(_ central: CBCentralManager, didConnect peripheral: CBPeripheral) {
+        present(UIAlertController(title: "OK", message: nil, preferredStyle: .alert), animated: true, completion: nil)
+        print("didConnect : \(peripheral.name ?? "UNKNOWN")")
+        peripheral.delegate = self
+        peripheral.discoverServices(nil)
+    }
+    
+    func centralManager(_ central: CBCentralManager, didFailToConnect peripheral: CBPeripheral, error: Error?) {
+        present(UIAlertController(title: "OK", message: error.debugDescription, preferredStyle: .alert), animated: true, completion: nil)
+        print("didFailToConnect : \(error.debugDescription)")
+    }
+    
+    func centralManagerDidUpdateState(_ central: CBCentralManager) {
+        discoveredPeripherals = []
+        centralManager?.scanForPeripherals(withServices: nil, options: nil)
+    }
+
+}
 
 class MasterViewController: UITableViewController {
-
+    
     var detailViewController: DetailViewController? = nil
     var objects = [Any]()
-
+    var centralManager: CBCentralManager?
+    var discoveredPeripherals: [Peripheral] = [] {
+        didSet {
+            tableView.reloadData()
+        }
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        centralManager = CBCentralManager(delegate: self, queue: nil, options: nil)
+
         // Do any additional setup after loading the view, typically from a nib.
         navigationItem.leftBarButtonItem = editButtonItem
 
@@ -42,15 +95,6 @@ class MasterViewController: UITableViewController {
     // MARK: - Segues
 
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        if segue.identifier == "showDetail" {
-            if let indexPath = tableView.indexPathForSelectedRow {
-                let object = objects[indexPath.row] as! NSDate
-                let controller = (segue.destination as! UINavigationController).topViewController as! DetailViewController
-                controller.detailItem = object
-                controller.navigationItem.leftBarButtonItem = splitViewController?.displayModeButtonItem
-                controller.navigationItem.leftItemsSupplementBackButton = true
-            }
-        }
     }
 
     // MARK: - Table View
@@ -60,14 +104,14 @@ class MasterViewController: UITableViewController {
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return objects.count
+        return discoveredPeripherals.count
     }
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
 
-        let object = objects[indexPath.row] as! NSDate
-        cell.textLabel!.text = object.description
+        let peripheral = discoveredPeripherals[indexPath.row]
+        cell.textLabel!.text = peripheral.cbPeripheral.name == nil ? "UNKNOWN" : peripheral.cbPeripheral.name
         return cell
     }
 
@@ -75,14 +119,18 @@ class MasterViewController: UITableViewController {
         // Return false if you do not want the specified item to be editable.
         return true
     }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        centralManager?.connect(discoveredPeripherals[indexPath.row].cbPeripheral, options: nil)
+    }
 
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            objects.remove(at: indexPath.row)
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view.
-        }
+//        if editingStyle == .delete {
+//            objects.remove(at: indexPath.row)
+//            tableView.deleteRows(at: [indexPath], with: .fade)
+//        } else if editingStyle == .insert {
+//            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view.
+//        }
     }
 
 
